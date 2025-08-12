b25f08d9156228228f7d8677f782109dcb86dd8a
Ivan Logvynenko <ivanlogvynenko2112@gmail.com>
2025-08-13 00:02:25 +0200

    some changes to build file

--- cmake/defaults.cmake
+++ cmake/defaults.cmake
@@ -6,6 +6,6 @@ set(CMAKE_C_COMPILER "clang")
 set(CMAKE_CXX_COMPILER "clang++")
 
 set(DEFAULT_CXX_STANDARD 23)
-set(DEFAULT_COMPILE_OPTIONS "-Wall" "-Wextra" "-Wpedantic" "-Wconversion" "-g")
+set(DEFAULT_COMPILE_OPTIONS "-Wall" "-Wextra" "-Wpedantic" "-Wconversion")
 
 set(DEFAULT_COMPILE_OPTIMIZATION "-O3")
