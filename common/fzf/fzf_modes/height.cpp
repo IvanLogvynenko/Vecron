e9bfc8364c0a59feebb16dc134dd68f567dccf42
Ivan Logvynenko <ivanlogvynenko2112@gmail.com>
2025-09-06 15:12:47 +0200

    use std::clamp

--- common/fzf/fzf_modes/height.cpp
+++ common/fzf/fzf_modes/height.cpp
@@ -1,18 +1,7 @@
 #include "height.hpp"
-#include <cstdint>
 #include <string>
 
 namespace fzf::mode {
-
-uint8_t
-Height::border(uint8_t value, uint8_t bottom, uint8_t up) const noexcept {
-    if (value < bottom)
-        return bottom;
-    else if (value > up)
-        return up;
-    else
-        return value;
-}
 
 Height::operator std::string() const noexcept {
     return "--height " + std::to_string(this->_height);
--- common/fzf/fzf_modes/height.hpp
+++ common/fzf/fzf_modes/height.hpp
@@ -1,18 +1,16 @@
 #pragma once
 #include "fzf/fzf_modes/fzf_mode.hpp"
+#include <algorithm>
 #include <cstdint>
 
 namespace fzf::mode {
 
 class Height : public FzfMode {
 private:
-    const uint8_t _height;
-
-    uint8_t
-    border(uint8_t value, uint8_t bottom = 0, uint8_t up = 100) const noexcept;
+    const int _height;
 
 public:
-    explicit Height(uint8_t height) : FzfMode(), _height(border(height)) {}
+    explicit Height(int height) : FzfMode(), _height(std::clamp(height, 0, 100)) {}
 
     operator std::string() const noexcept override;
 };
